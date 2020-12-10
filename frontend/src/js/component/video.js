b8e415d6f8cafbfa0cc4f4f833f5f3e75e4dcef3
xharris <xhh950@gmail.com>
2020-12-09 23:33:29 -0500

    removes blob

--- frontend/src/js/component/video.js
+++ frontend/src/js/component/video.js
@@ -8,32 +8,6 @@ const bss = block("video")
 const re_video_id = /api\/file\/(\w+)/
 
 const Video = ({ source, type, className, preview }) => {
-  const [url, setUrl] = useState()
-
-  useEffect(() => {
-    let is_mounted = true
-    if (source) {
-      const m = source.match(re_video_id)
-      if (m) {
-        const id = m[1]
-        apiFile.get(id).then(res => {
-          const enc = new TextEncoder()
-          const buf = enc.encode(res)
-          if (is_mounted) console.log(buf)
-          const bloburl = URL.createObjectURL(new Blob([res], { type }))
-          if (is_mounted) setUrl(source)
-        })
-      }
-    }
-    return () => {
-      is_mounted = false
-    }
-  }, [source])
-
-  useEffect(() => {
-    if (url) console.log(url)
-  }, [url])
-
   return (
     <video controls={!preview} className={cx(bss({ preview }), className)}>
       {source && <source src={source} />}
