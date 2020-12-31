568b5fa9170f5189a106951ad18b68ccfec2b886
xharris <xhh950@gmail.com>
2020-12-31 15:43:27 -0500

    some config changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const { backend } = require("./api")
 backend.start({
   name: "squeakr",
   port: 3000,
-  whitelist: [process.env.REACT_DB_HOST],
+  whitelist: [process.env.APP_HOST],
   skip_recursive_require: true
   //debug: true
 })
